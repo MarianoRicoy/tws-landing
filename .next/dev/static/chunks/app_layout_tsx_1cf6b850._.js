c170a5d5890399cf215e58864f3c3358a3f69a48
MarianoRicoy <mfricoy@gmail.com>
2026-02-19 16:28:04 -0300

    chore: Stop tracking .next directory and update gitignore

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8cbfea81._.js",
-  "static/chunks/[root-of-the-server]__78a57f8c._.css"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_tsx_16f857eb._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_1c30a0b7._.js",
-  "static/chunks/components_1cbd5898._.js"
-],
-    source: "dynamic"
-});
